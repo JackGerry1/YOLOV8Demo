166e1e134d96112e6a7cf13c17e4f832a722213e
jack <jackg2667@protonmail.com>
2024-10-03 18:02:22 +0100

    initial commit

---
+++ AlpacaTest-1/README.dataset.txt
@@ -0,0 +1,6 @@
+# AlpacaTest > 2024-09-30 2:42pm
+https://universe.roboflow.com/alpaca-test-dataset/alpacatest
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ main.py
@@ -0,0 +1,53 @@
+from ultralytics import YOLO
+import glob
+import os
+
+# Get the current working directory
+CURRENT_DIR = os.getcwd()
+
+# Define the image and save directories
+image_directory = f'{CURRENT_DIR}/AlpacaTest-1/test/images/'
+save_directory = f'{CURRENT_DIR}/AlpacaTest-1/predictions/'
+
+# Load the pretrained YOLOv8 model (recommended)
+model = YOLO('yolov8n.pt') 
+
+# Create a new YOLO model from scratch
+#model = YOLO('yolov8n.yaml') 
+
+# Train the model on custom dataset
+model.train(data=f'{CURRENT_DIR}/AlpacaTest-1/data.yaml', epochs=50, imgsz=640)
+
+# Train Model on Coco8 dataset
+#model.train(data='coco8.yaml', epochs=50, imgsz=640)
+
+# Validate the model
+model.val()
+
+# Create the save directory if it doesn't exist
+os.makedirs(save_directory, exist_ok=True)
+
+
+# Test On Single Image
+#results = model(
+#    source="https://ultralytics.com/images/bus.jpg", # this image can be changed 
+#   save=True, 
+#    project=save_directory, 
+#    name='.',        
+#    exist_ok=True    
+#)
+
+### Test On Multiple Images ###
+
+# Use glob to find all images in the directory
+image_paths = glob.glob(image_directory + '*.jpg') 
+
+# Loop through each image file and make predictions, saving them all in the same folder
+for image_path in image_paths:
+    results = model(
+        source=image_path, 
+        save=True, 
+        project=save_directory, 
+        name='.',        # Prevents creation of subfolders
+        exist_ok=True    # Allows saving in an existing directory
+    )
